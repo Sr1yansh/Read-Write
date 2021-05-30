90cd7736f6251fda6d1960ffa88aacfac82a9c29
Shriyansh <shriyansh.verma98@gmail.com>
2021-05-30 21:58:17 +0530

    First

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-  mongoURI:'mongodb://CHANGEME',
-  googleClientID:'CHANGEME',
-  googleClientSecret:'CHANGEME'
+   mongoURI : 'mongodb+srv://shiro:jaishriram@cluster0.toore.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  googleClientID: '315174641613-sr7i4cs7dccfhko08avgq4q7bve39esi.apps.googleusercontent.com',
+  googleClientSecret: 'RGXy-iBVBXAVDmAGIMKIFbqv'
 }
